6b34d2f4fff07fecc8dccc85736d870e694e26e8
gianmarioale <giammi.alessio@gmail.com>
2024-02-13 14:56:26 +0100

    first commit

---
+++ functions.py
@@ -0,0 +1,180 @@
+import torch
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
+import torchvision.transforms.functional as TF
+from torch import nn
+import random
+import numpy as np
+from fast_soft_sort.pytorch_ops import soft_sort
+
+
+def plot_images(images,label,n,columns,rows):
+    # columns*rows must be equal to n
+    # Prendi le prime 50 immagini del batch
+    images = images[:n]
+
+    # Crea una griglia di subplot
+    fig, axs = plt.subplots(rows, columns, figsize=(columns, rows))
+
+    for i, ax in enumerate(axs.flatten()):
+        # Rimuovi la dimensione del canale per la visualizzazione
+        image = images[i].squeeze()
+
+        # Converti il tensore in un array NumPy
+        image = image.numpy()
+
+        # Visualizza l'immagine
+        ax.imshow(image, cmap='gray')
+        ax.axis('off')  # Rimuovi gli assi
+        ax.set_title(label[i].numpy())
+
+    plt.tight_layout()
+    plt.show()
+
+def rotate_images(images, labels, angle=3):
+    rotated_images = []
+    rotated_labels = []
+    rotated_atoms = []
+    cardinality = int(360/angle)
+
+
+    for i in range(len(images)):
+
+        image = images[i]
+        label = labels[i]
+
+        if i == 0 :
+            for j in range(cardinality):
+
+                rotated_atom  = TF.rotate(image,angle*j)
+                rotated_atoms.append(rotated_atom)
+                rotated_images.append(rotated_atom)
+                rotated_labels.append(label)
+
+        # Choose a random rotation angle (multiple of 3 degrees)
+        degree = random.choice(range(0, 360, angle))
+        
+        # Rotate the image
+        rotated_image = TF.rotate(image, degree)
+        rotated_images.append(rotated_image)
+        rotated_labels.append(label)
+
+    rotated_atoms = torch.stack(rotated_atoms)
+    rotated_atoms = rotated_atoms.view(cardinality, -1)
+
+    return torch.stack(rotated_images), torch.tensor(rotated_labels), rotated_atoms, cardinality
+
+def imshow(img):
+    npimg = img.numpy()
+    plt.axis("off")
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+
+
+def soft_sort_approx(input, beta=1.0):
+    indices = torch.argsort(input)
+    probabilities = torch.softmax(beta * input, dim=-1)
+    return torch.sum(indices * probabilities, dim=-1)
+
+def plot_images_with_predictions(images, true_labels, predicted_labels, n, rows, columns):
+    # Ensure that columns*rows is equal to n
+    if columns * rows != n:
+        raise ValueError("The product of columns and rows must be equal to n")
+
+    # Take the first n images from the batch
+    images = images[:n]
+    true_labels = true_labels[:n]
+    predicted_labels = predicted_labels[:n]
+
+    # Create a grid of subplots
+    fig, axs = plt.subplots(rows, columns, figsize=(columns*2, rows*2))
+
+    for i, ax in enumerate(axs.flatten()):
+
+        # Reshape the image to 28x28 for visualization
+        image = images[i].view(28, 28).numpy()
+
+        # Display the image
+        ax.imshow(image, cmap='gray')
+        ax.axis('off')  # Remove the axes
+
+        # Set the title with true and predicted labels
+        title = f"True: {true_labels[i].item()}, Pred: {predicted_labels[i].item()}"
+        ax.set_title(title)
+
+    plt.tight_layout()
+    plt.show()
+
+def calculate_accuracy(model, data_loader):
+    correct_predictions = 0
+    total_predictions = 0
+
+    with torch.no_grad():
+        for data, labels in data_loader:
+
+            data=data.view(data.shape[0], -1)
+            outputs = model(data)
+            _, predicted = torch.max(outputs.data, 1)
+            
+            plot_images_with_predictions(data, labels, predicted, 5, 1, 5)
+
+            total_predictions += labels.size(0)
+            correct_predictions += (predicted == labels).sum().item()
+
+    accuracy = correct_predictions / total_predictions if total_predictions > 0 else 0
+    return accuracy
+
+def get_activations(_x, _model, _name):
+    activations = {}
+
+    def get_activation_hook(name):
+        def hook(_model, _input, _output):
+            _ = _model, _input
+            activations[name] = _output.detach()
+
+        return hook
+
+    layer = getattr(_model, _name)
+    layer.register_forward_hook(get_activation_hook("name"))
+    _ = _model(_x)
+    activation = activations["name"]
+    return activation
+
+def symm_loss(atoms, cardinality, sigma_squared=0.001):
+    
+    # Calcola la matrice dei prodotti scalari
+    scalar_products = torch.matmul(atoms, atoms.t())
+    
+    # Calcola la differenza dei prodotti scalari (sfrutta la proprietà del broadcasting)
+    diff_squared = scalar_products.unsqueeze(2) - scalar_products.unsqueeze(1)
+    diff_squared = diff_squared.pow(2)
+    
+    # Crea la matrice delta_ij
+    delta_ij = torch.eye(cardinality).unsqueeze(-1).unsqueeze(-1)
+    delta_ij = delta_ij.to(atoms.device)  # Assicurati che delta_ij sia sullo stesso dispositivo degli atomi
+    
+    # Calcola la loss
+    loss = (1 - delta_ij * cardinality) * torch.exp(-diff_squared / sigma_squared)
+    loss = loss.sum()
+    
+    return loss
+
+def comm_loss_function(parameters,train_images):
+
+    mean_data =train_images - train_images.mean(dim=0)
+    mean_paramters = parameters - parameters.mean(dim=0)
+
+    data_cov = torch.mm(mean_data.t(), mean_data)/ (train_images.shape[0] - 1)
+    parameters_cov = torch.mm(mean_paramters.t(), mean_paramters)/ (train_images.shape[0] - 1)
+    commutator = torch.mm(data_cov, parameters_cov) - torch.mm(parameters_cov, data_cov)
+    comm_loss = torch.norm(commutator)
+    return comm_loss
+
+def reg_loss_function(weights):
+
+    gramian = torch.mm(weights, weights.t()) # first layer weights
+    row_sorted = soft_sort(gramian, direction="DESCENDING", regularization_strength=1.0, regularization="l2")
+    col_sorted  = soft_sort(gramian.t(), direction="DESCENDING", regularization_strength=1.0, regularization="l2")
+    reg_loss = torch.sum((gramian - row_sorted)**2) + torch.sum((gramian - col_sorted)**2)
+
+    return reg_loss
---
+++ readme.md
@@ -0,0 +1,96 @@
+# Readme 
+
+## Advanced Topics in Machine Learning - 
+
+This is the first attempt to implement a Fully Connected 1 layer neural network (`number of neurons = 728`) multiclass classification problem that is invariant to the rotation of the MNIST dataset over a certain angle (in my analysis I selected an angle of 3 degree) and to compare the results with the same network but with different regularization terms, in particular:
+
+- the *FCNN* with only the cross entropy loss. (**The CE LOSS**)
+- the *FCNN* with the cross entropy loss and the regularization term for the challenge (**The Challenge LOSS**)
+- the *FCNN* with the cross entropy loss and the Symmetry-Adapted Regularization terms (**The Symmetry LOSS**)
+
+Most of the functions used are inside the file `./functions.py`
+
+The parameters used are:
+- `n_epochs = 1000`
+- `angle_rotation = 3 degree`
+- `cardinality = 120` (360/angle_rotation)
+- `batch_size_training = 6120` (6000 random rotated images + 120 atoms that is the orbit of 1 image selected randomly)
+- `barch_size_test = 1120` (1000 random rotated images + 120 atoms that is the orbit of 1 image selected randomly)
+- `learning_rate = 0.001`
+- `lambda_reg=0.01`
+- `lambda_symm=0.01`
+- `lambda_comm=0.01`
+- `lambda_symm_comm=0.01`
+- `sigma_squared=0.001` (in order to compute the symm_loss)
+
+Notes: 
+These results are obtained using the best model weights over 10 runs over 1000 epochs.
+The plots of the Loss functions and the weight matrices of first layer are saved inside the folder `./img`
+The weights of the best performing Loss for each analysis are saved in the folder `./best_model_weights`
+
+
+# The CE LOSS
+
+For this Loss it was mplemented only the [the cross entropy loss](https://pytorch.org/docs/stable/generated/torch.nn.CrossEntropyLoss.html) 
+
+- Train Accuracy: 73.51%
+- Test Accuracy: 53.66%
+- Invariance evaluation:
+  - Average Euclidean norm of the difference: 14.704 (16.758 control)
+  - **Difference: 2.054**
+- For 10 runs it tooks 1.5 minute and for the results we used the best recorded one
+
+# The Challenge LOSS
+
+For this Loss it was implemented the [the cross entropy loss](https://pytorch.org/docs/stable/generated/torch.nn.CrossEntropyLoss.html) and the regularization term as explained for the challenge. (The commutator term between the covariance matrix of the weigths and the data and the Gramian sorted term)
+
+- Train Accuracy: 58.68%
+- **Test Accuracy: 54.64%**
+- Invariance evaluation:
+  - Average Euclidean norm of the difference: 7.173 (7.900 control)
+  - Difference: 0.727
+- For 10 runs it tooks 10.40 minute and for the results we used the best recorded one
+
+# The Symmetry LOSS
+
+For this Loss it was implemented the [the cross entropy loss](https://pytorch.org/docs/stable/generated/torch.nn.CrossEntropyLoss.html) and the regularization term as explained in [the paper](https://arxiv.org/abs/2006.14027).
+
+- **Train Accuracy: 73.72%**
+- Test Accuracy: 53.21%
+- Invariance evaluation:
+  - Average Euclidean of the difference: 14.753 (16.801 control) 
+  - Difference: 2.048
+- For 10 runs it tooks **19.16 minute** and for the results we used the best recorded one
+
+::: {layout-ncol=2}
+![](./img/cross_entropy_loss.png)
+
+![Cross Entropy Weight Matrices 10x784](./img/cross_entropy_loss_weights.png)
+
+![](./img/challenge_loss.png)
+
+![Challenge Weight Matrices 10x784](./img/challenge_loss_weights.png)
+
+![](./img/symmetry_loss.png)
+
+![Symmetry Weight Matrices 10x784](./img/symmetry_loss_weights.png)
+
+:::
+
+# Conclusion e further investigations
+
+This initial exploration into the application of symmetry regularization within a Fully Connected Neural Network (FCNN) framework on the MNIST dataset has provided invaluable insights into the operational intricacies of neural networks and the practical use of PyTorch. It has illuminated the profound impact that symmetries have on the regularization of weight matrices, as evidenced by the significant rotational symmetries observed in the weight matrices from our experiments. This investigation has allowed for a nuanced understanding of how loss functions behave and their resultant effects on both training and testing accuracies. The adoption of a **Symmetry Regularization framework** has been instrumental in shedding light on how to effectively address sample complexity challenges, while also revealing areas that require further exploration to fully leverage this framework's potential.
+
+Looking ahead, several critical questions remain unanswered, signifying the need for further investigation:
+
+**Parameter Sensitivity Analysis**: Understanding the intricate relationship between various hyperparameters and their impact on the loss function's behavior and, subsequently, on the model's predictive accuracy.
+
+**Generalization Across Diverse Image Sets**: Assessing the model's robustness and generalization capabilities when the training set (MNIST for Orbit classification) and a distinctly different test set (comprising entirely new images with the same dimensionality and color scale) are used. This inquiry aims to understand how variations in the test dataset impact the model's accuracy, providing insights into the model's adaptability to new, unseen data.
+
+**Extended Framework Applications and Beyond**: Exploring additional aspects of symmetry-adapted regularization in FCNNs and other model architectures.
+
+# References
+- [1] Teacher lectures (2023-2024): Advanced Topics in Machine Learning
+- [2] [Symmetry-Adapted Regularization for Learning invariance in Neural Networks](https://arxiv.org/abs/2006.14027)
+- [3] [Equivariant Neural Networks](https://dmol.pub/dl/Equivariant.html#equivariant-neural-networks-%20with-constraints)
+- [4] Github code 
